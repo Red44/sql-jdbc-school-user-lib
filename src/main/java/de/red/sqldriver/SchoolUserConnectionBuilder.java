7c173513ec702bbda1531c0348849ac6e3bf34d4
red <jakob.hillers@gmail.com>
2022-05-24 01:22:15 +0200

    [code] student interface added

---
+++ src/main/java/de/red/sqldriver/SchoolUserConnectionBuilder.java
@@ -0,0 +1,31 @@
+package de.red.sqldriver;
+
+import de.red.sqldriver.impl.PostgreSQLConnection;
+import java.sql.Connection;
+
+public class SchoolUserConnectionBuilder {
+
+  String name;
+  String pwd;
+  public SchoolUserConnectionBuilder(){
+
+  }
+  public SchoolUserConnectionBuilder appendName(String name){
+    this.name = name;
+    return this;
+  }
+
+  public SchoolUserConnectionBuilder appendPassword(String pwd){
+    this.pwd = pwd;
+    return this;
+  }
+
+  public Connection build(String name){
+    if(name == null|| pwd == null){
+      throw new RuntimeException("Name or password is missing!");
+    }
+    PostgreSQLConnection con = new PostgreSQLConnection(name,pwd,"redslaboratory.tech");
+    con.connect(name);
+    return con.getConnection();
+  }
+}
